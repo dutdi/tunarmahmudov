9f35c927c759c1bf6a45725ad63339418af5d6cd
Tunar Mahmudov <tunardev14@gmail.com>
2025-06-24 14:28:13 +0400

    fix contact

--- src/components/contactMeSection/ContactForm.jsx
+++ src/components/contactMeSection/ContactForm.jsx
@@ -19,8 +19,8 @@ const ContactForm = () => {
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs
-      .sendForm("service_ko3hmpt", "template_ahbmmqd", form.current, {
-        publicKey: "I6HAT5mUZH7WHabGE",
+      .sendForm("service_79mpb6g", "template_dpc6owm", form.current, {
+        publicKey: "GqORSaXapdbQTkbSd",
       })
       .then(
         () => {

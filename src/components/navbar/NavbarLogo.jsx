a0c1c249622466706962961b7820b30af6f6141b
Tunar Mahmudov <tunardev14@gmail.com>
2025-06-24 15:11:14 +0400

    fix home button click

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,11 +1,11 @@
 const NavbarLogo = () => {
   return (
     <div>
-      <a href="/" className="hidden md:block">
+      <a href="/tunarmahmudov/" className="hidden md:block">
         <img src="/tunarmahmudov/images/logo.png" alt="logo" className="h-14" />
       </a>
 
-      <a href="/" className="block md:hidden">
+      <a href="/tunarmahmudov/" className="block md:hidden">
         <img src="/tunarmahmudov/images/logo.png" alt="logo" className="h-14" />
       </a>
     </div>
